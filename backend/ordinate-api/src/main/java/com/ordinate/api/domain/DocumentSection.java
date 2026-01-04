aa89695d74b66d4fd14eb0fd5d895742117d3bf1
deveshmur <dmdevesh@gmail.com>
2026-01-04 17:59:18 -0500

    Modify documentsection

--- backend/ordinate-api/src/main/java/com/ordinate/api/domain/DocumentSection.java
+++ backend/ordinate-api/src/main/java/com/ordinate/api/domain/DocumentSection.java
@@ -1,5 +1,32 @@
 package com.ordinate.api.domain;
 
+import java.time.Instant;
+import java.util.UUID;
+
 public class DocumentSection {
-    
+    private final UUID id;
+    private final Instant createdAt;
+    private Instant lastModifiedAt;
+
+    public DocumentSection() {
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
+        this.lastModifiedAt = this.createdAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getLastModifiedAt() {
+        return lastModifiedAt;
+    }
+
+    public void touch() {
+        this.lastModifiedAt = Instant.now();
+    }
 }
