4ae71d09ae2058ec988eb872f82689da52490935
deveshmur <dmdevesh@gmail.com>
2026-01-04 18:39:35 -0500

    Modify templateresponsedto to include key

--- backend/ordinate-api/src/main/java/com/ordinate/api/dto/TemplateResponseDto.java
+++ backend/ordinate-api/src/main/java/com/ordinate/api/dto/TemplateResponseDto.java
@@ -1,11 +1,14 @@
 package com.ordinate.api.dto;
 
+import com.ordinate.api.domain.TemplateKey;
+
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
 public record TemplateResponseDto(
         UUID id,
+        TemplateKey key,
         Instant createdAt,
         Instant lastModifiedAt,
         List<TemplateSectionResponseDto> sections
