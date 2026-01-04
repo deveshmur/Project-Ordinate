3f35741ffe9662b60d030ebe50d0108bdb64211d
deveshmur <dmdevesh@gmail.com>
2026-01-04 17:16:43 -0500

    backend files set

---
+++ backend/ordinate-api/src/main/java/com/ordinate/api/OrdinateApiApplication.java
@@ -0,0 +1,13 @@
+package com.ordinate.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdinateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdinateApiApplication.class, args);
+	}
+
+}
