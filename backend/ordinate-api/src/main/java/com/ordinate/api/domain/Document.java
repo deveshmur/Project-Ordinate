973382f96b9f9f427a46a311750c84282f1c82d9
deveshmur <dmdevesh@gmail.com>
2026-01-04 17:52:24 -0500

    Modify document

--- backend/ordinate-api/src/main/java/com/ordinate/api/domain/Document.java
+++ backend/ordinate-api/src/main/java/com/ordinate/api/domain/Document.java
@@ -1,5 +1,34 @@
 package com.ordinate.api.domain;
 
+import java.time.Instant;
+import java.util.UUID;
+
+
 public class Document {
-    
+
+    private final UUID id;
+    private final Instant createdAt;
+    private Instant lastModifiedAt;
+
+    public Document() {
+        this.id = UUID.randomUUID();
+        this.createdAt = Instant.now();
+        this.lastModifiedAt = this.createdAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getLastModifiedAt() {
+        return lastModifiedAt;
+    }
+
+    public void touch() {
+        this.lastModifiedAt = Instant.now();
+    }
 }
