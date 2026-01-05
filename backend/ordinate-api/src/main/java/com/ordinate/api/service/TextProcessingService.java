437116237860144833d6ff7b8c428f850bf6d39d
deveshmur <dmdevesh@gmail.com>
2026-01-04 19:27:34 -0500

    Modify textproessingservice for sections to match template

--- backend/ordinate-api/src/main/java/com/ordinate/api/service/TextProcessingService.java
+++ backend/ordinate-api/src/main/java/com/ordinate/api/service/TextProcessingService.java
@@ -2,6 +2,7 @@ package com.ordinate.api.service;
 
 import com.ordinate.api.domain.Document;
 import com.ordinate.api.domain.DocumentSection;
+import com.ordinate.api.domain.Template;
 import com.ordinate.api.dto.DocumentResponseDto;
 import com.ordinate.api.dto.ProcessTextRequestDto;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class TextProcessingService {
 
+    private final TemplateService templateService;
+
+    public TextProcessingService(TemplateService templateService) {
+        this.templateService = templateService;
+    }
+
     public DocumentResponseDto processText(ProcessTextRequestDto request) {
+        Template template = templateService.getTemplate(request.templateKey());
+
         Document document = new Document();
-        DocumentSection section = new DocumentSection();
-        document.addSection(section);
+
+        template.getSections().forEach(ts -> document.addSection(new DocumentSection()));
 
         return new DocumentResponseDto(
                 document.getId(),
